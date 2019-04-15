cd9be3b2cb8ac7710d8cde7a5d90965da4fae4d2
Dawood Ikhlaq <daudmalik06@gmail.com>
2019-04-15 15:41:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,10 @@
 <?php
-// developed by Red V!per
-error_reporting(0);
 
 include("CMS_Detector.class.php");
-
-if (isset($argv)) {
-    $url = $argv[1];
-}
-
-$opts = array('http' =>
-    array(
-        'method'  => 'GET',
-        'timeout' => 10 
-         )
-);
-
-$context=stream_context_create($opts);
-
-$content=file_get_contents($url, false, $context);
-
-
-//$content=file_get_contents($url);
-
+$content=file_get_contents($url);
 $apps=CMS_Detector::process($content);
 foreach($apps as $app)
 {
 	$url=CMS_Detector::appToURL($app);
-	echo "<a href='$url'>$app</a>";
-	echo "<br />";
+	echo "$app".PHP_EOL;
 }
-
-?>
